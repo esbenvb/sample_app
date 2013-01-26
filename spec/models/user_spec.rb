2298ef77ac53b2f3f68ddcc95cde6b90d7aaeef9
Esben von Buchwald <esbenvb@989064.no-reply.drupal.org>
2013-01-26 17:47:25 +0100

    done exersizes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -59,6 +59,15 @@ describe User do
     end
     it {should_not be_valid}
   end
+  
+  describe "email address with mixed case" do
+    let (:mixed_case_email) { "fdsfASDf.e2@fasdfASD.dk" }
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+     @user.save
+     @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
 
   describe "when password is not present" do
     before { @user.password = @user.password_confirmation = ' '  }
