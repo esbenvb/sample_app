fbf43dce096e8d87c721217af165c42bbe997ddb
Esben von Buchwald <esbenvb@989064.no-reply.drupal.org>
2013-01-30 00:01:31 +0100

    finished user handling

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -39,3 +39,10 @@ def valid_signin(user)
 end
 
 
+def sign_in(user)
+  visit signin_path
+  fill_in "Email", with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+  cookies[:remember_token] = user.remember_token
+end
