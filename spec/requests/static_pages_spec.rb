fd1b967eec9750e1a9032c5ce10bf989bcb2645a
Esben von Buchwald <esbenvb@989064.no-reply.drupal.org>
2013-01-31 23:47:19 +0100

    add user microposts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -16,6 +16,23 @@ describe "Static pages" do
     let (:page_title) { '' }
     it_should_behave_like "all static pages" 
     it { page.should_not have_title("| Home") }
+
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user)}
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          page.should have_selector("li##{item.id}", text: item.content)
+        end
+      end
+    end
+
   end
   describe "Help page" do
     before { visit help_path }
@@ -51,5 +68,4 @@ describe "Static pages" do
     click_link "sample app"
     page.should have_title  full_title('')
   end
-  
 end
